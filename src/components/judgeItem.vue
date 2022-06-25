aee490afb9dbe4bb4e8c0d192e771d55ae9a9956
yuzehang <yuzehang@qq.com>
2022-06-25 11:15:25 +0800

    judge

---
+++ src/components/judgeItem.vue
@@ -0,0 +1,41 @@
+<template >
+  <div>
+    <h2>{{ question.title }}</h2>
+    <el-radio-group v-model="radio">
+      <el-radio label="1">是</el-radio>
+      <el-radio label="0">否</el-radio>
+    </el-radio-group>
+    <br />
+    <br />
+    <el-button type="primary" @click="test">提交</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "judgeItem",
+  question: [],
+  props: ["name", "question"],
+  data() {
+    return {
+      radio: "F",
+      id: 0,
+    };
+  },
+
+  methods: {
+    //测试回答是否正确
+    test: function () {
+      if (this.radio == "F") {
+        return;
+      }
+      if (this.radio == this.question.ans) {
+        this.$message.success("答对啦");
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div v-for="item in questions" :key="item.id">
+    <div v-for="item in judgequestions" :key="item.id">
+      <judgeItemVue :question="item"></judgeItemVue>
+    </div>
+    <div v-for="item in choicequestions" :key="item.id">
       <TestItem :question="item"></TestItem>
     </div>
   </div>
@@ -9,24 +12,35 @@
 <script>
 import api from "@/request/network";
 import TestItem from "@/components/TestItem.vue";
+import judgeItemVue from "@/components/judgeItem.vue";
 export default {
-  components: { TestItem },
+  components: { TestItem, judgeItemVue },
   data() {
     return {
-      questions: [],
+      choicequestions: [],
+      judgequestions: [],
       id: 0,
     };
   },
   created() {
     //console.log("hehe sb");
-    for (var i = 1; i <= this.questions; i++) {
+    for (var i = 1; i <= this.choicequestions; i++) {
       console.log(i);
     }
     api
       .get(`/test/getQuestion`)
       .then((data) => {
         console.log(data);
-        this.questions = data.data.testInfo;
+        this.choicequestions = data.data.testInfo;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    api
+      .get(`/judgeTest/get`)
+      .then((data) => {
+        console.log(data);
+        this.judgequestions = data.data.judgeText;
       })
       .catch((err) => {
         console.log(err);
