26538ce8ea293cc7b1f69d2672ded0bbb8041bc2
runkon <84058318+dustincosmos@users.noreply.github.com>
2022-06-25 15:30:32 +0800

    视图
    cjy

--- src/views/PointawardView.vue
+++ src/views/PointawardView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="block">
     <span class="demonstration">直接前往</span>
-    <el-table :data="tableData" style="width: 100%">
+    <el-table :data="tableData" style="width: 100%" align="center">
       <el-table-column prop="name" label="知识点" width="180" align="center">
       </el-table-column>
       <el-table-column
